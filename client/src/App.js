f11c2586760f73fd747b92a5144cb240ad2157d7
Marcin Kaczmarek <marcin.kaczmarek@przelewy24.pl>
2024-07-05 15:24:45 +0200

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,29 @@
 // src/App.js
-import './App.css';
-import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-import About from './pages/About'; // Importowanie komponentu About z folderu pages
+import "./App.css";
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import About from "./pages/About"; // Importowanie komponentu About z folderu pages
 
 const Home = () => <h2>Homes</h2>;
 const Users = () => <h2>Userssss</h2>;
 
 function App() {
   return (
-    <div className="bg-gray-500">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/users">Users</Link>
-          </li>
-        </ul>
+    <div>
+      <nav className="w-100 h-20 bg-gray-500 flex justify-center">
+        <div className="flex gap-x-40">
+          <Link to="/">Home</Link>
+          <Link to="/about">UUID</Link>
+          <Link to="/users">RSA</Link>
+          <Link to="/password-generator">Password generator</Link>
+        </div>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/users" element={<Users />} />
+        {/* <Route path="/password-generator" element={<PasswordGenerator />} /> */}
       </Routes>
     </div>
   );
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,19 +4,8 @@ import React from 'react';
 const About = () => {
   return (
     <div style={{ padding: '20px' }}>
-      <h1>About Us</h1>
-      <p>Welcome to the About page. We are a team of passionate developers building awesome applications with React.</p>
-      <h2>Our Mission</h2>
-      <p>Our mission is to create high-quality, user-friendly web applications that solve real-world problems and enhance user experience.</p>
-      <h2>Our Team</h2>
-      <ul>
-        <li><strong>John Doe</strong> - Full Stack Developer</li>
-        <li><strong>Jane Smith</strong> - Frontend Developer</li>
-        <li><strong>Emily Johnson</strong> - UX/UI Designer</li>
-        <li><strong>Michael Brown</strong> - Backend Developer</li>
-      </ul>
-      <h2>Contact Us</h2>
-      <p>If you have any questions or feedback, feel free to <a href="mailto:info@example.com">contact us</a>.</p>
+      <h1>Generate your UUID </h1>
+
     </div>
   );
 }
