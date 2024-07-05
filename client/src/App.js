325faa2242c18a101768a39756fb2eb42829e726
Marcin Kaczmarek <marcin.kaczmarek@przelewy24.pl>
2024-07-05 14:58:34 +0200

    added tailwind and config

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+// src/App.js
 import './App.css';
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import About from './pages/About'; // Importowanie komponentu About z folderu pages
+
+const Home = () => <h2>Homes</h2>;
+const Users = () => <h2>Userssss</h2>;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-500">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
     </div>
   );
 }
